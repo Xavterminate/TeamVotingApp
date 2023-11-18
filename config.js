7e115fda4574205ba490b037ab886af79ac81440
Xavterminate <dclewis73@gmail.com>
2023-11-18 18:10:59 +1000

    Changed style and firebase

--- config.js
+++ config.js
@@ -1,9 +1,16 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyAiLG7XWy-6sAwexXPTPYUWx_KSov7MYJ4",
+    authDomain: "teamvotingapp-66a0f.firebaseapp.com",
+    databaseURL: "https://teamvotingapp-66a0f-default-rtdb.firebaseio.com",
+    projectId: "teamvotingapp-66a0f",
+    storageBucket: "teamvotingapp-66a0f.appspot.com",
+    messagingSenderId: "966933909188",
+    appId: "1:966933909188:web:5d9c1190a4bca296044391",
+    measurementId: "G-595HS7V5NH"
+  };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
